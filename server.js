de8ccee21f8c371c0788e0584dacc724bf903ba3
luizakar2002 <51238018+luizakar2002@users.noreply.github.com>
2023-07-19 13:51:03 +0400

    gpt API integration

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const { Configuration, OpenAIApi } = require("openai")
+
+const config = new Configuration({
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(config);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/chat", async (req, res)=>{
+    const {prompt} = req.body;
+
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        max_tokens: 512,
+        temperature: 0,
+        prompt: prompt,
+    });
+    res.send(completion.data.choices[0].text);
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
